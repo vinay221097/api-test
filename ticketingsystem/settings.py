aa78e64e153630915debe2574185edbf0dce46cd
vinay221097 <vinay221097@gmail.com>
2021-01-25 16:09:02 +0100

    second commit

--- ticketingsystem/settings.py
+++ ticketingsystem/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!h2*2(!%i8&d=eb#t!^7rweblk7)%k9_59+_@g3j79gmz7=lb0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api-test22.herokuapp.com']
 
 
 # Application definition
